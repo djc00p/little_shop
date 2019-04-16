548a61028d122996d9dc016c7dd4bf5977f702d5
Deonte Cooper <deonte.cooper12@gmail.com>
2019-04-16 14:33:27 -0600

    Added passing tests for Merchant To-Do list

--- app/controllers/dashboard/dashboard_controller.rb
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Dashboard::DashboardController < Dashboard::BaseController
   def index
     @merchant = current_user
     @pending_orders = Order.pending_orders_for_merchant(current_user.id)
+    @revenue = Order.missing_revenue(current_user.id)
+    # @order_items = @pending_orders.order_items_for_merchant(@merchant.id)
   end
 end
